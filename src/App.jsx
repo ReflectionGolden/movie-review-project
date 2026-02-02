e76c4208cf9bb1132e2110d7f4b88451c61b289c
Golden_Reflection <reflectiongolden@gmail.com>
2026-02-02 14:10:11 +0000

    second part of initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css'
 
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+
 function App() {
 
   return (
     <>
       <h1>Movie Review Application</h1>
       <p>This project has been made in conjunction with the <a title='GitHub Pages site' href="https://reflectiongolden.github.io/react-essentials-site/" target='_blank'>react-essentials-site</a><a title='GitHub Repository' href="https://github.com/ReflectionGolden/react-essentials-site" target='_blank'><FontAwesomeIcon icon={faGithub} /></a> project as part of Module 9.</p>
+      <Container>
+        <h2>Temp Header</h2>
+        <p>Lorem ipsum</p>
+      </Container>
     </>
   )
+}
+
+function Container(props) {
+  return (
+    <div className='mt-4 mb-4 rounded-4xl border-3 border-solid border-orange-100'>
+      {props.children}
+    </div>
+  );
 }
 
 export default App
